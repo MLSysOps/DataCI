c7f3a124c28a933e3391a1ab6e8a0b573de9ac11
yuanmingleee <yli056@e.ntu.edu.sg>
2023-02-21 01:12:16 +0800

    :sparkles: Add Repo for management DataCI repository

---
+++ dataci/repo/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: Li Yuanming
+Email: yuanmingleee@gmail.com
+Date: Feb 21, 2023
+"""
+import os
+from pathlib import Path
+
+DATACI_DIR = '.dataci'
+
+
+class Repo(object):
+    DATACI_DIR = '.dataci'
+
+    def __init__(self):
+        self.root_dir = self.find_root()
+
+    @property
+    def dataset_dir(self):
+        return self.root_dir / 'dataset'
+
+    @property
+    def pipeline_dir(self):
+        return self.root_dir / 'pipeline'
+
+    @staticmethod
+    def find_root(root=None):
+        root = Path(root or os.curdir)
+        root_dir = root.resolve()
+
+        if not root_dir.is_dir():
+            raise FileNotFoundError(f"directory '{root}' does not exist")
+
+        while True:
+            dataci_dir = root_dir / DATACI_DIR
+            if dataci_dir.is_dir():
+                return root_dir
+            # Reach the FS root
+            if root_dir.parent == root_dir:
+                break
+            root_dir = root_dir.parent
+
+        raise FileNotFoundError("you are not inside of a DVC repository")
