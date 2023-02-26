1527f5f0b03d2c6fca06e23807c05a8f2c22ced7
yuanmingleee <yli056@e.ntu.edu.sg>
2023-02-27 01:53:52 +0800

    :sparkles: [pipeline] Add stage input resolver

--- dataci/pipeline/stage.py
+++ dataci/pipeline/stage.py
@@ -7,39 +7,54 @@ Date: Feb 20, 2023
 """
 import logging
 from abc import ABC, abstractmethod
+from distutils.dir_util import copy_tree
 from pathlib import Path
+from shutil import copyfile
 
 from dataci.dataset import list_dataset
+from dataci.repo import Repo
 
 logger = logging.getLogger(__name__)
 
 
 class Stage(ABC):
-    def __init__(self, name: str, inputs: str, outputs: str, dependency='auto') -> None:
+    def __init__(self, name: str, inputs: str, outputs: str, dependency='auto', repo=None) -> None:
         self.name = name
-        self.inputs = inputs
+        self._inputs = inputs
         self.outputs = outputs
         self.dependency = dependency
+        self.repo = repo or Repo()
 
-    def resolve_inputs(self, inputs):
+    @property
+    def inputs(self):
         """Resolve shortcut init argument"""
         logger.debug(f'Resolving stage {self.name}...')
         # Resolve input: find the dataset path
         # try: input is published dataset
-        logger.debug(f'Try resolve inputs {inputs} as published dataset')
-        datasets = list_dataset(self.repo, inputs, tree_view=False)
+        logger.debug(f'Try resolve inputs {self._inputs} as published dataset')
+        datasets = list_dataset(self.repo, self._inputs, tree_view=False)
         if len(datasets) == 1:
-            # Load dataset version at temp folder from DVC file cache
-            datasets[0].dataset_files
+            self._inputs = datasets[0].dataset_files
+            return self._inputs
 
         # try: input is a local file
-        logger.debug(f'Try resolve inputs {inputs} as local file')
-        inputs = Path(inputs)
-        if inputs.is_file() and inputs.exists():
-            # Copy input files to temp folder
-            inputs
+        logger.debug(f'Try resolve inputs {self._inputs} as local file')
+        if Path(self._inputs).exists():
+            return self._inputs
+        raise ValueError(f'Unable to resolve inputs {self._inputs} for stage {self.name}.')
+
+    @inputs.setter
+    def inputs(self, new_inputs):
+        if new_inputs == self._inputs:
             return
-        raise ValueError(f'Unable to resolve inputs {inputs} for stage {self.name}.')
+        # Set the inputs to a new path will trigger an auto copy inputs files to new path
+        inputs = Path(self._inputs)
+        if inputs.exists():
+            if inputs.is_dir():
+                copy_tree(str(inputs), new_inputs)
+            else:
+                copyfile(inputs, new_inputs)
+        self._inputs = new_inputs
 
     def resolve_outputs(self):
         # Resolve outputs
