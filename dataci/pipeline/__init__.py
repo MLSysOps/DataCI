a44b791221d8779a60648abb6443f92f0178891a
YuanmingLeee <YuanmingLeee@gmail.com>
2023-03-01 20:08:24 +0800

    :sparkles: [feat] Add decorator for pipeline stage

--- dataci/pipeline/__init__.py
+++ dataci/pipeline/__init__.py
@@ -5,7 +5,8 @@ Author: Li Yuanming
 Email: yuanmingleee@gmail.com
 Date: Feb 20, 2023
 """
+import decorator
 from .pipeline import Pipeline
 from .stage import Stage
 
-__all__ = ['Pipeline', 'Stage']
+__all__ = ['Pipeline', 'Stage', 'decorator']
--- example/train_classification_pipeline/train_text_classification_pipeline.py
+++ example/train_classification_pipeline/train_text_classification_pipeline.py
@@ -5,46 +5,44 @@ import pandas as pd
 import unicodedata
 from cleantext import clean
 
-from dataci.pipeline import Stage, Pipeline
+from dataci.pipeline import Pipeline, Stage
+from dataci.pipeline.decorator import stage
 
 
-class TextClean(Stage):
-    @staticmethod
-    def clean(text):
-        # remove emoji, space, and to lower case
-        text = clean(text, to_ascii=False, lower=True, normalize_whitespace=True, no_emoji=True)
+def clean_func(text):
+    # remove emoji, space, and to lower case
+    text = clean(text, to_ascii=False, lower=True, normalize_whitespace=True, no_emoji=True)
 
-        # remove accent
-        text = ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
+    # remove accent
+    text = ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
 
-        return text
-
-    def run(self, inputs):
-        df = pd.read_csv(os.path.join(inputs, 'train.csv'))
-        df['to_product_name'] = df['to_product_name'].map(self.clean)
-        return df
+    return text
 
 
-class TextAugmentation(Stage):
-    def run(self, inputs):
-        df = pd.read_csv(inputs)
-        transform = txtaugs.InsertPunctuationChars(
-            granularity="all",
-            cadence=5.0,
-            vary_chars=True,
-        )
-        df['to_product_name'] = df['to_product_name'].map(transform)
-        return df
+@stage(name='text_clean', inputs='pairwise_raw[train]', outputs='text_clean.csv')
+def text_clean(inputs):
+    df = pd.read_csv(os.path.join(inputs, 'train.csv'))
+    df['to_product_name'] = df['to_product_name'].map(clean_func)
+    return df
 
 
-text_clean = TextClean('text_clean', inputs='pairwise_raw[train]', outputs='text_clean.csv')
-text_augmentation = TextAugmentation('text_augmentation', inputs='text_clean.csv', outputs='text_aug.csv')
+@stage(name='text_augmentation', inputs='text_clean.csv', outputs='text_augmentation.csv')
+def text_augmentation(inputs):
+    df = pd.read_csv(inputs)
+    transform = txtaugs.InsertPunctuationChars(
+        granularity="all",
+        cadence=5.0,
+        vary_chars=True,
+    )
+    df['to_product_name'] = df['to_product_name'].map(transform)
+    return df
+
 # Debug run
 print('Debug run')
 print('Running text clean')
 text_clean()
 print('Running text augmentation')
-# text_augmentation()
+text_augmentation()
 # Define a pipeline
 print('Define a pipeline')
 pipeline = Pipeline('train_text_classification')
