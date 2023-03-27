31fd7dfbd066900834f529d008dc33159d64ffe0
yuanmingleee <yli056@e.ntu.edu.sg>
2023-03-27 23:58:10 +0800

    :tada: [init] Init data debugger structure

---
+++ dataci/data_debugger/data_flow_trace.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: Li Yuanming
+Email: yuanmingleee@gmail.com
+Date: Mar 27, 2023
+"""
+
+
+def trace(dataset_identifier: str):
+    # Get dataset
+
+    # Get benchmark result
+
+    # Visualize data flow
+    pass
