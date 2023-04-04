01e957c972cdeb2195f02f4e7f046bde9d3222bd
yuanmingleee <yli056@e.ntu.edu.sg>
2023-04-05 00:21:31 +0800

    :tada: [data investigate] Init data investigation code structure

---
+++ dataci/data_debugger/data_investigator/statistical_analysis.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: Li Yuanming
+Email: yuanmingleee@gmail.com
+Date: Apr 04, 2023
+"""
+
+
+def feature_distribution(feature):
+    """feature distribution"""
+    pass
+
+
+def statistics():
+    """Get statistics of a dataset
+    Min, max, mean, median, mode, standard deviation, variance, skewness, kurtosis
+    """
+    pass
