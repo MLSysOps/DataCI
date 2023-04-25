6f82dcdffa641a33ce86d85c78f64185f2ad7f50
yuanmingleee <yli056@e.ntu.edu.sg>
2023-04-26 01:22:12 +0800

    :sparkles: [workspace] Mount s3 directory to local

--- dataci/connector/s3.py
+++ dataci/connector/s3.py
@@ -6,10 +6,14 @@ Email: yuanmingleee@gmail.com
 Date: Apr 25, 2023
 """
 import configparser
+import logging
 
+import boto3 as boto3
 import s3fs
 
-from dataci import CONFIG_FILE
+from dataci import CONFIG_FILE, CACHE_ROOT
+
+logger = logging.getLogger(__name__)
 
 
 def connect(key=None, secret=None, endpoint_url=None):
@@ -27,6 +31,9 @@ def connect(key=None, secret=None, endpoint_url=None):
     if section_name not in config:
         config.add_section(section_name)
     s3_config = config[section_name]
+    # copy original user provided credentials
+    key_original, secret_original, endpoint_url_original = key, secret, endpoint_url
+
     key = key or s3_config.get('key', '')
     secret = secret or s3_config.get('secret', '')
     endpoint_url = endpoint_url or s3_config.get('endpoint_url', '')
@@ -37,15 +44,60 @@ def connect(key=None, secret=None, endpoint_url=None):
     endpoint_url_ = None if not endpoint_url else endpoint_url
 
     # test connection
-    s3 = s3fs.S3FileSystem(anon=False, key=key_, secret=secret_, endpoint_url=endpoint_url_)
-    s3.ls('/')
+    fs = s3fs.S3FileSystem(anon=False, key=key_, secret=secret_, endpoint_url=endpoint_url_)
+    fs.ls('/')
 
-    # save credentials to config file
-    config.set(section_name, 'key', key)
-    config.set(section_name, 'secret', secret)
-    config.set(section_name, 'endpoint_url', endpoint_url)
-    with open(CONFIG_FILE, 'w') as configfile:
-        config.write(configfile)
+    logger.info('S3 connection established.')
+
+    if any(map(lambda x: x is not None, [key_original, secret_original, endpoint_url_original])):
+        # if any of the credentials is provided, then we need to save the credentials to config file.
+        # save credentials to config file
+        config.set(section_name, 'key', key)
+        config.set(section_name, 'secret', secret)
+        config.set(section_name, 'endpoint_url', endpoint_url)
+        with open(CONFIG_FILE, 'w') as configfile:
+            config.write(configfile)
+        # Also save to ~/.dataci/.passwd-s3fs
+        s3fs_password_file = CACHE_ROOT / '.passwd-s3fs'
+        # make sure the file is readable only by the owner
+        s3fs_password_file.touch(exist_ok=True, mode=0o600)
+        with open(s3fs_password_file, 'w') as f:
+            f.write(f'{key}:{secret}')
+        logger.info('S3 credentials saved to config file.')
+
+    return fs
+
+
+def create_bucket(bucket_name: str, region: str = 'ap-southeast-1'):
+    """Create a new S3 bucket.
+
+    Args:
+        bucket_name: Name of the bucket to be created.
+        region: Region to create the bucket in. If not set then the default region for DataCI (ap-southeast-1)
+            will be used.
+
+    TODO:
+        - Add support for other regions, better save/read the region in the config file.
+    """
+    # Read AWS credentials from config file
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    section_name = 'connector.s3'
+    key = config.get(section_name, 'key')
+    secret = config.get(section_name, 'secret')
+
+    # Create an S3 client
+    s3 = boto3.client(
+        's3', aws_access_key_id=key, aws_secret_access_key=secret, region_name=region
+    )
+
+    # Create the bucket
+    try:
+        location = {'LocationConstraint': region}
+        s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
+        logging.info(f"Bucket {bucket_name} created successfully.")
+    except Exception as e:
+        logging.error(f"Error creating bucket {bucket_name}: {e}")
 
 
 if __name__ == '__main__':
--- dataci/workspace/__init__.py
+++ dataci/workspace/__init__.py
@@ -6,8 +6,10 @@ Email: yuanmingleee@gmail.com
 Date: Feb 21, 2023
 """
 import os
+import subprocess
 
 from dataci import CACHE_ROOT
+from dataci.connector.s3 import connect as connect_s3, create_bucket
 
 
 class Workspace(object):
@@ -35,3 +37,20 @@ def create_or_use_workspace(workspace: Workspace):
     os.makedirs(workspace.root_dir, exist_ok=True)
     os.makedirs(workspace.workflow_dir, exist_ok=True)
     os.makedirs(workspace.data_dir, exist_ok=True)
+    # Create a S3 bucket for data
+    fs = connect_s3()
+    # Add a prefix to bucket name to avoid name conflict
+    # TODO: change the prefix to account specific
+    bucket_name = 'dataci-' + workspace.name
+    if not fs.exists(f'/{workspace.name}'):
+        create_bucket(bucket_name)
+    # Mount the bucket to local data directory
+    cmd = ['s3fs', f'{bucket_name}', f'{workspace.data_dir}', '-o', f'passwd_file={CACHE_ROOT / ".passwd-s3fs"}'
+                                                                    '-o', 'url=https://s3.ap-southeast-1.amazonaws.com']
+    subprocess.run(cmd, check=True)
+
+    # Init database
+    from dataci.db import init  # noqa
+
+    # Set the current workspace as the default workspace
+    os.environ['DATACI_WORKSPACE'] = workspace.name
