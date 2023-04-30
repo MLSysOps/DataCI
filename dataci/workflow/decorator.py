8b5da336e34f3d856087d3a3e3ecea0673af27ea
yuanmingleee <yli056@e.ntu.edu.sg>
2023-04-30 17:25:18 +0800

    :sparkles: [workflow] Simplify workflow stage

--- dataci/workflow/decorator.py
+++ dataci/workflow/decorator.py
@@ -5,26 +5,19 @@ Author: Li Yuanming
 Email: yuanmingleee@gmail.com
 Date: Mar 1, 2023
 """
+from functools import wraps
 from typing import Callable
 
 from .stage import Stage
 
 
-def stage(inputs, outputs, name=None, dependency='auto', repo=None) -> Callable[[Callable], Stage]:
+def stage(name=None) -> Callable[[Callable], Stage]:
     """Pipeline stage decorator. Convert the wrapped function into a
     :code:`dataci.pipeline.stage.Stage` object.
-
-    Args:
-        inputs (str or os.PathLike): Stage inputs dataset name or data path.
-        outputs (str os os.PathLike): Stage outputs dataset name or data path.
-        name (str): Stage name. Default to `None`, which will infer from the name of wrapped function.
-        dependency (str or list of str): Dependencies of the stage. Default to 'auto', which will
-            automatically applied all the stage inputs and the stage's code.
-        repo (dataci.repo.Repo): Dataci Repo. Default to `None`, which will automatically infer the
-            repo location.
     """
 
     def decorator_stage(run):
+        @wraps(run)
         def wrapper_stage():
             stage_name = name or run.__name__
             # Convert stage name to camel case
@@ -35,9 +28,11 @@ def stage(inputs, outputs, name=None, dependency='auto', repo=None) -> Callable[
                 name_camel_case, (Stage,), {'run': lambda self, *args, **kwargs: run(*args, **kwargs)},
             )
             # Initiate the stage object with configured info (e.g., inputs, outputs, etc.)
-            stage_obj: Stage = stage_cls(
-                name=stage_name, inputs=inputs, outputs=outputs, dependency=dependency, repo=repo
-            )
+            stage_obj: Stage = stage_cls(name=stage_name)
             return stage_obj
-        return wrapper_stage()
+
+        wrapped_func = getattr(wrapper_stage, '__wrapped__')
+        stage_obj = wrapper_stage()
+        stage_obj.__wrapped__ = wrapped_func
+        return stage_obj
     return decorator_stage
--- dataci/workflow/stage.py
+++ dataci/workflow/stage.py
@@ -7,164 +7,65 @@ Date: Feb 20, 2023
 """
 import inspect
 import logging
-import os
-import pickle
 from abc import ABC, abstractmethod
-from pathlib import Path
-from typing import TYPE_CHECKING
 
-import pandas as pd
-from dataci.repo import Repo
-
-from dataci.db.dataset import get_one_dataset
-
-if TYPE_CHECKING:
-    from typing import Iterable, List
-    from dataci.dataset.dataset import Dataset
+from dataci.workspace import Workspace
 
 logger = logging.getLogger(__name__)
 
 
 class Stage(ABC):
     def __init__(
-            self, name: str, inputs: str, outputs: str, dependency='auto',
-            repo=None, code_base_dir=os.curdir, feat_base_dir=os.curdir
+            self, name: str,
     ) -> None:
-        self.repo = repo or Repo()
-        self.code_base_dir = Path(code_base_dir)
-        self.feat_base_dir = Path(feat_base_dir)
-        self.name = name
-        self._inputs = inputs
-        self._outputs = outputs
-        if dependency != 'auto' and not isinstance(dependency, Iterable):
-            self._dependency = [dependency]
-        else:
-            self._dependency = dependency
-
-    @property
-    def inputs(self) -> 'Dataset':
-        from dataci.dataset.dataset import Dataset
-
-        prev_deps = getattr(self, '__inputs_deps', None)
-        current_deps = (self._inputs, str(self.feat_base_dir))
-        if prev_deps != current_deps:
-            try:
-                # If input is a published dataset
-                split_result = self._inputs.split('@')
-                if len(split_result) == 1:
-                    name, version = split_result[0], 'latest'
-                else:
-                    name, version = split_result
-                dataset_dict = get_one_dataset(name=name, version=version)
-                dataset_dict['repo'] = self.repo
-                inputs = Dataset.from_dict(dataset_dict)
-            except ValueError:
-                # input is an intermedia feature
-                inputs = self.feat_base_dir / self._inputs
-            setattr(self, '__inputs_deps', current_deps)
-            setattr(self, '__inputs_cache', inputs)
-        return getattr(self, '__inputs_cache')
-
-    @property
-    def outputs(self) -> 'Dataset':
-        from dataci.dataset.dataset import Dataset
-
-        prev_deps = getattr(self, '__outputs_deps', None)
-
-        current_deps = (self._outputs, str(self.feat_base_dir))
-        if prev_deps != current_deps:
-            try:
-                # If output is a published dataset
-                # FIXME: this will cause a bug when pipeline is updated, bind pipeline stage to some dataset version
-                #   is not a good idea
-                dataset_dict = get_one_dataset(name=self._outputs)
-                dataset_dict['repo'] = self.repo
-                outputs = Dataset.from_dict(dataset_dict)
-            except ValueError:
-                # input is an intermedia feature, pack it into a dataset object
-                outputs = self.feat_base_dir / self._outputs
-            setattr(self, '__outputs_deps', current_deps)
-            setattr(self, '__outputs_cache', outputs)
-        return getattr(self, '__outputs_cache')
-
-    @property
-    def dependency(self):
-        # Resolve dependencies
-        if self._dependency == 'auto':
-            return [self.inputs] + self.__get_serialize_files()
-        else:
-            return self._dependency
+        workspace_name, task_name = name.split('.') if '.' in name else (None, name)
+        self.workspace = Workspace(workspace_name)
+        self.name = task_name
+        # context is set by workflow
+        self.context = dict()
 
     @abstractmethod
-    def run(self, inputs) -> 'List[Path]':
+    def run(self, inputs):
         raise NotImplementedError('Method `run` not implemented.')
 
-    def __get_serialize_files(self):
-        return [
-            (self.code_base_dir / self.name).with_suffix('.pkl'),
-            (self.code_base_dir / self.name).with_suffix('.py')
-        ]
+    @property
+    def script(self):
+        # Get the source code of the class
+        try:
+            source_code = inspect.getsource(self.__class__)
+        except OSError:
+            # If the source code is not available, the class is probably dynamically generated
+            # We can get the source code from the wrapped function
+            source_code = inspect.getsource(getattr(self, '__wrapped__'))
+        return source_code
 
-    def serialize(self):
-        """Pack current stage object to re-buildable files
+    def dict(self):
+        return dict(
+            name=self.name,
+            workspace=self.workspace.name,
+            script=self.script,
+            cls_name=self.__class__.__name__,
+        )
 
-        TODO: Pack using code instead of pickle
-        """
-        import cloudpickle
+    @classmethod
+    def from_dict(cls, config: dict):
+        config['name'] = f'{config["workspace"]}.{config["name"]}'
+        # Build class object from script
+        # TODO: make the build process more secure with sandbox / allowed safe methods
+        local_dict = locals()
+        exec(config['script'], globals(), local_dict)
+        sub_cls = local_dict[config['cls_name']]
+        self = sub_cls(name=config['name'])
+        return self
 
-        file_dict = dict()
-        file_dict[(self.code_base_dir / self.name).with_suffix('.pkl')] = cloudpickle.dumps(self)
-        # Generate run stage execution file
-        file_dict[(self.code_base_dir / self.name).with_suffix('.py')] = inspect.cleandoc(
-            f"""import pickle
-            from pathlib import Path
-
-
-            with open(Path(__file__).with_suffix('.pkl'), 'rb') as f:
-                stage_obj = pickle.load(f)
-            stage_obj()
-            """
-        ).encode()
-        return file_dict
-
-    @staticmethod
-    def deserialize(entry_file):
-        """Deserialize stage object from file or byte string
-        TODO: Load from code instead of pickle
-        """
-        obj_pkl = Path(entry_file).with_suffix('.pkl')
-        with open(obj_pkl, 'rb') as f:
-            stage_obj: Stage = pickle.load(f)
-            return stage_obj
-
-    def input_deserializer(self, inputs):
-        if os.path.splitext(inputs)[-1] == '.csv':
-            logger.info(f'Load input {inputs} as pandas Dataframe')
-            return pd.read_csv(inputs)
-        else:
-            raise ValueError(f'Not support input file type: {inputs}')
-
-    def output_serializer(self, outputs, dest):
-        """Output serializer to save the output data/feature."""
-        if outputs is None:
-            return
-        if isinstance(outputs, pd.DataFrame):
-            logger.info(f'Save output to {dest}')
-            outputs.to_csv(dest, index=False)
-        else:
-            raise ValueError(f'Not support output object type: {type(outputs)}.')
-
-    def __call__(self):
-        from dataci.dataset.dataset import Dataset
-
-        if isinstance(self.inputs, Dataset):
-            input_path = str(self.inputs.dataset_files)
-        else:
-            input_path = str(self.inputs)
-        inputs = self.input_deserializer(input_path)
-        outputs = self.run(inputs)
-        if isinstance(self.outputs, Dataset):
-            output_path = str(self.outputs.dataset_files)
-        else:
-            output_path = str(self.outputs)
-        self.output_serializer(outputs, output_path)
+    def __call__(self, inputs=None):
+        kwargs = dict()
+        if inputs is not None:
+            kwargs.update(inputs)
+        # Inspect run method signature
+        sig = inspect.signature(self.run)
+        # If context is in run method signature, pass context to run method
+        if 'context' in sig.parameters:
+            kwargs.update(self.context)
+        outputs = self.run(**kwargs)
+        return outputs
