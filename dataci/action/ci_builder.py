9f02bf8990705e62def6a4027664325ad095aed3
yuanmingleee <yli056@e.ntu.edu.sg>
2023-05-09 00:01:22 +0800

    :sparkles: [ci] Add CI builder

--- dataci/action/ci_builder.py
+++ dataci/action/ci_builder.py
@@ -5,11 +5,54 @@ Author: Li Yuanming
 Email: yuanmingleee@gmail.com
 Date: May 05, 2023
 """
-from dataci.models.workflow import Workflow
+import re
+
+from dataci.models import Stage, Workflow
+from dataci.opteraotrs.execute_workflow_operator import ExecuteWorkflowOperator
+from dataci.opteraotrs.shell_command_operator import ShellCommandOperator
 
 
-def build_ci_workflow(job_config: dict):
-    pass
+def build_ci_workflow(config: dict):
+    build_steps = list()
+    for step_config in config['jobs']['steps']:
+        step_name = step_config['name'].replace(' ', '_')
+        if step_config['uses']:
+            matched = re.match(r'(\w+)/(\w+)/(\w+)(?:@([\da-f]+))?', step_config['uses'])
+            if matched:
+                workspace_name, id_type, name, version = matched.groups()
+            else:
+                raise ValueError(f'Invalid step config: {step_config}, error at uses: {step_config["uses"]}')
+            # If id_type is workflows:
+            if id_type == 'workflows':
+                workflow_identifier = f'{workspace_name}.{name}@{version}'
+                step = ExecuteWorkflowOperator(name=step_name, workflow_identifier=workflow_identifier)
+                # Update parameters
+                step.params.update(step_config.get('with', dict()))
+            elif id_type == 'stages':
+                step = Stage.get(f'{workspace_name}.{name}@{version}')
+                # Update parameters
+                step.params.update(step_config.get('with', dict()))
+            else:
+                raise ValueError(f'Invalid step config: {step_config}, error at uses: {step_config["uses"]}')
+
+        # If step is a run step
+        elif step_config['run']:
+            step = ShellCommandOperator(name=step_name, command=step_config['run'])
+        else:
+            raise ValueError(f'Invalid step config: {step_config}')
+
+        build_steps.append(step)
+
+    # Build workflow
+    workflow = Workflow(
+        name=config['name'],
+        params=config['config'],
+    )
+    with workflow:
+        step0 = build_steps.pop(0)
+        for step in build_steps:
+            step0 = step0 >> step
+    workflow.publish()
 
 
 def build_ci_trigger_workflow(ci_config: dict):
@@ -24,5 +67,12 @@ def build_ci_trigger_workflow(ci_config: dict):
         workflow_template.schedule.append(f'@event {producer} {event} success')
 
     # Config params
-    workflow_template.params['config'] = ci_config['config']
+    workflow_template.params = ci_config['config']
 
+    # Publish workflow
+    workflow_template.publish()
+
+
+def build(ci_config: dict):
+    build_ci_workflow(ci_config)
+    build_ci_trigger_workflow(ci_config)
