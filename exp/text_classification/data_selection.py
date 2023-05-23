be0bd2d0a6e14b2248997a989a68b0a10fd3e3b7
YuanmingLeee <yuanmingleee@gmail.com>
2023-05-23 08:48:31 +0000

    :test_tube: [exp] Add data selection exp script

--- exp/text_classification/data_selection.py
+++ exp/text_classification/data_selection.py
@@ -8,14 +8,23 @@ Date: May, 23, 2023
 1. Download dataset from Google Drive
 2. Unzip dataset to current directory / dataset base directory
 """
+import logging
+import time
 from datetime import datetime
+from multiprocessing import Process
+from pathlib import Path
 
 import pandas as pd
 from alaas.server import Server
+from alaas.client import Client
+from alaas.server.executors import TorchALWorker
 from dateutil.relativedelta import relativedelta
+from docarray import Document, DocumentArray
 
 # DATASET_BASE_PATH = '/data/yelp/reviews_{}.csv'
-DATASET_BASE_PATH = 'reviews_{}.csv'
+DATASET_BASE_PATH = 'data/reviews_{}.csv'
+
+logger = logging.getLogger(__name__)
 
 
 def read_dataset(start_date: str, end_date: str):
@@ -31,11 +40,55 @@ def read_dataset(start_date: str, end_date: str):
     return pd.concat(df_list, ignore_index=True)
 
 
-if __name__ == '__main__':
-    Server.start(
-        model_hub="huggingface/pytorch-transformers",
+def select_data(df, num_samples: int, strategy: str = 'RandomSampling'):
+    text_list = df['text'].tolist()
+
+    # Large number of data breaks the server, let's do in a non-server-client way
+
+    # Start ALaaS server in a separate process
+    logger.info('Start AL Worker')
+    al_worker = TorchALWorker(
         model_name="bert-base-uncased",
-        tokenizer="bert-base-uncased",
-        transformers_task="text-classification",
-        strategy="RandomSampling"
+        model_repo="huggingface/pytorch-transformers",
+        device='cuda',
+        strategy=strategy,
+        minibatch_size=8,
+        tokenizer_model="bert-base-uncased",
+        task="text-classification"
     )
+
+    # Prepare data for ALWorker
+    doc_list = []
+    for txt in text_list:
+        doc_list.append(Document(text=txt, mime_type='text'))
+
+    queries = al_worker.query(
+        DocumentArray(doc_list),
+        parameters={'budget': num_samples, 'n_drop': None}
+    ).to_list()
+    query_df = pd.DataFrame(queries, columns=['text'])
+
+    # Get back selected rows
+    logger.info('Get selected rows by selected text')
+    selected_df = df.merge(query_df, on='text', how='inner')
+
+    return selected_df
+
+
+def save_dataset(df, file_path):
+    Path(file_path).parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(file_path, index=False)
+
+
+if __name__ == '__main__':
+    exp_time = '2021Q1'
+    data_start_date = '202010'
+    data_end_date = '202012'
+    data_selection_method = 'RS'
+    strategy_name_mapper = {
+        'RS': 'RandomSampling',
+    }
+
+    df_ = read_dataset(start_date=data_start_date, end_date=data_end_date)
+    selected_df = select_data(df_, num_samples=50_000, strategy=strategy_name_mapper[data_selection_method])
+    save_dataset(selected_df, f'processed/data_select_{exp_time}_{data_selection_method}.csv')
