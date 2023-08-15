e62539262c33db4f17a86bbc2aff7ff337c8f2e8
Li, Yuanming <your AliWork ID@alibaba-inc.com>
2023-08-15 11:41:55 +0800

    :fire: [clean] Remove unused redundant files

--- dataci/action/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Author: Li Yuanming
-Email: yuanmingleee@gmail.com
-Date: Apr 24, 2023
-"""
--- dataci/opteraotrs/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Author: Li Yuanming
-Email: yuanmingleee@gmail.com
-Date: 5月 08, 2023
-"""
